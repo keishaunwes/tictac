9c3fa4537ee7d98309c08e6dcd8c0b20022324b2
keishaun wesley <keishaunwesley@gmail.com>
2020-01-14 16:14:38 -0600

    hard but fun mostly hard

--- tictac.js
+++ tictac.js
@@ -1,1 +1,17 @@
-let rowss = document.querySelector(".row");
+let rowss = document.querySelectorAll(".row");
+
+rowss.forEach(function(rows) {
+  rows.addEventListener("click", tictac);
+});
+
+let currentPlayer = 0;
+
+function tictac(e) {
+  if (currentPlayer % 2 == 0) {
+    e.target.textContent = "X";
+    currentPlayer++;
+} else {
+    e.target.textContent = "O";
+    currentPlayer++;
+}
+}
